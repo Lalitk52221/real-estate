3fc392d5468b84949847b3105928a7e92d7566f9
Lalit Kumar <Lalitk52221@outlook.com>
2026-07-12 22:28:25 +0530

    Update gap spacing in Categories component for better layout consistency

--- components/home/Categories/index.tsx
+++ components/home/Categories/index.tsx
@@ -27,7 +27,7 @@ export default function Categories() {
 
         </div>
 
-        <div className="grid grid-cols-3 gap-8 lg:grid-cols-5">
+        <div className="grid grid-cols-3 gap-4 md:gap-8 lg:grid-cols-5">
           {categories.map((item) => (
             <CategoryCard
               key={item.title}
